c8f13b31c0297edcda13a0da8ddf8fdbb2748794
John Koehn <kingkoehn@gmail.com>
2017-05-28 19:32:28 -0500

    inital setup

---
+++ StockTrainer/StockTrainer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StockTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
